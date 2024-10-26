02e3f2b574b7de9eb042527444f0d15e4c8b5388
alelu-pellereca <alejandrapellegrini@gmail.com>
2024-10-26 13:38:27 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World FLY!' });
+});
+
+app.post('/echo', (req, res) => {
+    res.json(req.body);
+});
+
+// Frontend
+app.get('/frontend', (req, res) => {
+    res.json({ image: 'alepellereca/imagenfront:latest', description: 'Frontend' });
+});
+
+// Backend
+app.get('/backend', (req, res) => {
+    res.json({ image: 'alepellereca/imagenback:latest', description: 'Backend' });
+});
+
+// Función para iniciar el servidor en un puerto específico
+const startServer = (port, description) => {
+    const server = express();
+    server.get('/', (req, res) => {
+        res.json({ message: `Server running on port ${port}: ${description}` });
+    });
+    
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+};
+
+startServer(3001, 'Frontend');
+startServer(8081, 'Backend');
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const app = require('./app'); // Ruta correcta a tu app.js
+
+describe('API Tests', () => {
+    it('should return Hello World on GET /', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Hello World FLY!');
+    });
+
+    it('should echo back the posted data on POST /echo', async () => {
+        const data = { name: 'Test User' };
+        const response = await request(app).post('/echo').send(data);
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(data);
+    });
+
+    it('Frontend', async () => {
+        const response = await request(app).get('/frontend');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            image: 'alepellereca/imagenfront:latest',
+            description: 'Frontend',
+        });
+    });
+
+    it('Backend', async () => {
+        const response = await request(app).get('/backend');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            image: 'alepellereca/imagenback:latest',
+            description: 'Backend',
+        });
+    });
+});
